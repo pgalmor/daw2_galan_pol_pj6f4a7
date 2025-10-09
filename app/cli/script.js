94a8bc7e1e3cc64cac3236d4c64cb1f7a5fb9e8e
pgalmor <pgalmor@gmail.com>
2025-10-09 18:20:33 +0200

    some changes

--- app/cli/script.js
+++ app/cli/script.js
@@ -1,3 +1,17 @@
 function NewOrder(){
     
 }
+
+function ViewOrder(code){
+    document.getElementById('order');
+}
+
+function ViewOrders(){
+    document.getElementById('orders');
+}
+
+window.onload = function(){
+    if(document.getElementById('orders') != null){
+        ViewOrders();
+    }
+}
